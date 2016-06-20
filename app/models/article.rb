93176ff217c8c615215180c0780e1b1757c5b96a
levantien83 <levantien83@gmail.com>
2016-06-21 02:25:36 +0700

    adding comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true
+	validates :body, presence: true
 	def self.search(search)
 		if search
 			where(["lower(title) LIKE ?" , "%#{search.downcase}%"])
