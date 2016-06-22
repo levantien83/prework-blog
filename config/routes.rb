3af2ecb3ce3400b6f5b38be969ea317601c05285
levantien83 <levantien83@gmail.com>
2016-06-22 23:59:06 +0700

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'articles#index'
+  root :to => 'articles#index'
   get '/articles/hashtag/:name', to: 'articles#hashtag'
 
   # The priority is based upon order of creation: first created -> highest priority.
